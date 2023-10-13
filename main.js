aa93d719089591b6d106bb697ac79309c94254fe
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-10-13 18:57:19 -0300

    cria logica de responder o player conforme o acerto da cor

--- main.js
+++ main.js
@@ -7,25 +7,41 @@ function createRgb() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-// insere rgb num paragrafo
-
-function insertRgb() {
-  const p = document.getElementById('rgb-color');
-  p.innerText = createRgb();
-}
-
 // insere paletas de cores na página
 function insertColorGuess() {
   const div = document.getElementById('paleta');
+  const p = document.getElementById('rgb-color');
+  p.innerText = createRgb();
+  const random = Math.floor(Math.random() * 6);
   for (let i = 0; i < 6; i += 1) {
     const divCircle = document.createElement('div');
     divCircle.className = 'ball';
-    divCircle.style.backgroundColor= createRgb();
+    if (random === i) {
+      divCircle.style.backgroundColor = p.innerText;
+    } else {
+      divCircle.style.backgroundColor = createRgb();
+    }
     div.appendChild(divCircle);
   }
 }
 
+function responseGame() {
+  const ball = document.getElementsByClassName('ball');
+  const p = document.getElementById('rgb-color');
+  const answer = document.getElementById('answer');
+  for (let i = 0; i < ball.length; i += 1) {
+    ball[i].addEventListener('click', (event) => {
+      const backGround = event.target.style.backgroundColor;
+      if (backGround === p.innerText) {
+        answer.innerText = 'Acertou!';
+      } else {
+        answer.innerText = 'Errou! Tente novamente!';
+      }
+    });
+  }
+}
+
 window.onload = () => {
-  insertRgb();
   insertColorGuess();
+  responseGame();
 };
